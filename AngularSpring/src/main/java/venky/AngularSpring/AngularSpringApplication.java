23dd0d23a75990dd3ea879ec9b08564b392449f9
Trinadh9533 <trinathvenky9951@gmail.com>
2022-11-12 21:45:38 +0530

    Code change

--- AngularSpring/src/main/java/venky/AngularSpring/AngularSpringApplication.java
+++ AngularSpring/src/main/java/venky/AngularSpring/AngularSpringApplication.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @CrossOrigin(origins = "*")
 public class AngularSpringApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args ) {
 		SpringApplication.run(AngularSpringApplication.class, args);
 	}
 
